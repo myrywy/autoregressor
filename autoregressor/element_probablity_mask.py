177c014b8fff5cfcaac1127c201f9f7120ff2259
Marcin Lewandowski <marcinlewy22@gmail.com>
2018-12-15 22:44:43 +0100

    Add alternative options for alternative values of masked elements in ElemetProbabilityMasking

--- autoregressor/element_probablity_mask.py
+++ autoregressor/element_probablity_mask.py
@@ -9,17 +9,17 @@ class ElementProbabilityMasking(tf.keras.layers.Layer):
             id_lower_limit, # inclusive 
             id_upper_limit, # exclusive
             element_id_to_index_in_probability_distribution_mapping=tf.identity,
+            masked_value=0,
             ):
         """
-        This class is work in progress.
-
-        Semantyka jest taka: allowed zawiera kolejne listy z dozwolony id-ki ze słownika dla danego elementu tj.
+        Allowed zawiera kolejne listy z dozwolony id-ki ze słownika dla danego elementu tj.
         allowed_values[0] <- lista możliwych elementów na pozycji zerowej, wszystkie elementy, których nie ma na tej liści
         będą miały zerowe prawdopodobieństwo. Wyjątek: jeśli lista jest pusta to oznacza, że wszytkie elementy są możliwe
-        probability_distribution_size - szerokość rozkładu prawdopodobieństwa 
-        id_lower_limit (inclusive)
-        id_upper_limit (exclusive)
-
+        Args:
+            probability_distribution_size - szerokość rozkładu prawdopodobieństwa 
+            id_lower_limit (inclusive)
+            id_upper_limit (exclusive)
+            masked_value: "min" - masked values becomes eqal to min value in whole input tensor; "<min" - masked values becomes smaller than min value in whole input tensor; or numerical value - masked values becomes this number;  
         """
         super(ElementProbabilityMasking, self).__init__()
         self.element_id_to_index_in_probability_distribution_mapping = element_id_to_index_in_probability_distribution_mapping
@@ -47,6 +47,9 @@ class ElementProbabilityMasking(tf.keras.layers.Layer):
         mask = tf.SparseTensor(indices, values, dense_shape)
         self.mask = tf.sparse_tensor_to_dense(mask)
         
+        if isinstance(masked_value, str) and masked_value not in ("min", "<min"):
+            raise ValueError("Masked value must be numerical value or 'min' or '<min'.")
+        self.masked_value = masked_value
 
     def call(self, probabilites, step=None):
         """
@@ -55,5 +58,14 @@ class ElementProbabilityMasking(tf.keras.layers.Layer):
         step - number of element in sequence (sentence) that is being predicted, assuming that first predicted is 1"""
         
         mask_at_step = tf.gather(self.mask, step - 1, name="gather_mask_for_steps")
-        return probabilites * mask_at_step
+        mask_at_step = tf.cast(mask_at_step, dtype=tf.bool)
+        if isinstance(self.masked_value, str):
+            min_value = tf.reduce_min(tf.reshape(probabilites, (-1,),name="flatten"), name="get_mininum")
+            masked_values = tf.ones_like(probabilites) * min_value
+            if self.masked_value == "<min":
+                masked_values = masked_values - tf.ones_like(masked_values)
+        else:
+            masked_values = tf.ones_like(probabilites) * self.masked_value
+        
+        return tf.where(mask_at_step, probabilites, masked_values)
         
--- autoregressor/test_element_probability_mask.py
+++ autoregressor/test_element_probability_mask.py
@@ -65,3 +65,204 @@ def test_call(allowed, expected_mask, steps, mask_slice):
         r_masked = sess.run(masked)
     expected_masked = probabilities * np.array(expected_mask[mask_slice])
     assert expected_masked == approx(r_masked)
+
+
+input_distribution_int = tf.constant(
+    [
+        [1,-2,3],
+        [4,-5,6],
+        [7,-8,9]
+    ],
+    dtype=tf.int32
+)
+
+input_distribution_float = tf.constant(
+    [
+        [0.1,-0.2,0.3],
+        [0.4,-0.5,0.6],
+        [0.7,-0.8,0.9]
+    ],
+    dtype=tf.float32
+)
+
+
+@pytest.mark.parametrize("input, allowed_ids, masked_value, step, expected_output",
+    [
+        (input_distribution_float, [[],[],[]], 0, [1,1,1],
+            np.array(
+                [
+                    [0.1,-0.2,0.3],
+                    [0.4,-0.5,0.6],
+                    [0.7,-0.8,0.9]
+                ],
+                dtype=np.float32
+            )
+        ), 
+        (input_distribution_int, [[],[],[]], 0, [1,1,1],
+            np.array(
+                [
+                    [1,-2,3],
+                    [4,-5,6],
+                    [7,-8,9]
+                ],
+                dtype=np.int32
+            )
+        ), 
+        (input_distribution_float, [[],[1],[0,2]], 16, [2,2,2],
+            np.array(
+                [
+                    [0.1,16,0.3],
+                    [0.4,16,0.6],
+                    [0.7,16,0.9]
+                ],
+                dtype=np.float32
+            )
+        ), 
+        (input_distribution_int, [[],[1],[0,2]], 16, [2,2,2],
+            np.array(
+                [
+                    [1,16,3],
+                    [4,16,6],
+                    [7,16,9]
+                ],
+                dtype=np.int32
+            )
+        ), 
+        (input_distribution_float, [[],[1],[0,2]], "min", [0,0,0],
+            np.array(
+                [
+                    [0.1,-0.2,0.3],
+                    [0.4,-0.5,0.6],
+                    [0.7,-0.8,0.9]
+                ],
+                dtype=np.float32
+            )
+        ), 
+        (input_distribution_int, [[],[1],[0,2]], "min", [0,0,0],
+            np.array(
+                [
+                    [1,-2,3],
+                    [4,-5,6],
+                    [7,-8,9]
+                ],
+                dtype=np.int32
+            )
+        ), 
+        (input_distribution_float, [[],[1],[0,2]], "min", [2,2,2],
+            np.array(
+                [
+                    [0.1,-0.8,0.3],
+                    [0.4,-0.8,0.6],
+                    [0.7,-0.8,0.9]
+                ],
+                dtype=np.float32
+            )
+        ), 
+        (input_distribution_int, [[],[1],[0,2]], "min", [2,2,2],
+            np.array(
+                [
+                    [1,-8,3],
+                    [4,-8,6],
+                    [7,-8,9]
+                ],
+                dtype=np.int32
+            )
+        ), 
+        (input_distribution_float, [[],[1],[0,2]], "min", [1,1,1],
+            np.array(
+                [
+                    [-0.8,-0.2,-0.8],
+                    [-0.8,-0.5,-0.8],
+                    [-0.8,-0.8,-0.8]
+                ],
+                dtype=np.float32
+            )
+        ), 
+        (input_distribution_int, [[],[1],[0,2]], "min", [1,1,1],
+            np.array(
+                [
+                    [-8,-2,-8],
+                    [-8,-5,-8],
+                    [-8,-8,-8]
+                ],
+                dtype=np.int32
+            )
+        ), 
+
+
+        (input_distribution_float, [[],[1],[0,2]], "<min", [0,0,0],
+            np.array(
+                [
+                    [0.1,-0.2,0.3],
+                    [0.4,-0.5,0.6],
+                    [0.7,-0.8,0.9]
+                ],
+                dtype=np.float32
+            )
+        ), 
+        (input_distribution_int, [[],[1],[0,2]], "<min", [0,0,0],
+            np.array(
+                [
+                    [1,-2,3],
+                    [4,-5,6],
+                    [7,-8,9]
+                ],
+                dtype=np.int32
+            )
+        ), 
+        (input_distribution_float, [[],[1],[0,2]], "<min", [2,2,2],
+            np.array(
+                [
+                    [0.1,-1.8,0.3],
+                    [0.4,-1.8,0.6],
+                    [0.7,-1.8,0.9]
+                ],
+                dtype=np.float32
+            )
+        ), 
+        (input_distribution_int, [[],[1],[0,2]], "<min", [2,2,2],
+            np.array(
+                [
+                    [1,-9,3],
+                    [4,-9,6],
+                    [7,-9,9]
+                ],
+                dtype=np.int32
+            )
+        ), 
+        (input_distribution_float, [[],[1],[0,2]], "<min", [1,1,1],
+            np.array(
+                [
+                    [-1.8,-0.2,-1.8],
+                    [-1.8,-0.5,-1.8],
+                    [-1.8,-0.8,-1.8]
+                ],
+                dtype=np.float32
+            )
+        ), 
+        (input_distribution_int, [[],[1],[0,2]], "<min", [1,1,1],
+            np.array(
+                [
+                    [-9,-2,-9],
+                    [-9,-5,-9],
+                    [-9,-8,-9]
+                ],
+                dtype=np.int32
+            )
+        ), 
+    ]
+)
+def test_call_non_default_values(input, allowed_ids, masked_value, step, expected_output):
+    DISTRIBUTION_SIZE = 3
+    t_step = tf.convert_to_tensor(step) + 1
+    mask = ElementProbabilityMasking(
+        allowed_ids, 
+        DISTRIBUTION_SIZE, 
+        0, 
+        DISTRIBUTION_SIZE, 
+        tf.identity, 
+        masked_value=masked_value)
+    t_output = mask.call(input, t_step)
+    with tf.Session() as sess:
+        r_output = sess.run(t_output)
+    assert r_output == approx(expected_output)
