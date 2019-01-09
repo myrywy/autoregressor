53ecb05b3499ba8bb7f2001f6ab793c3932ef9d2
Marcin Lewandowski <marcinlewy22@gmail.com>
2019-01-09 09:49:25 +0100

    move regression test for lm_training_data to separate module

--- autoregressor/lm_training_data.py
+++ autoregressor/lm_training_data.py
@@ -45,27 +45,3 @@ class LanguageModelTrainingData:
         return dataset
 
 
-
-import pytest
-from pytest import approx
-
-def test_regression_load_training_data():
-    td = LanguageModelTrainingData("glove300", "simple_examples", "data/simple_examples/cached_glove300/train_trimmed_to_40_tokens/", 5)
-    dataset = td.load_training_data()
-    it = dataset.make_initializable_iterator()
-    next_example = it.get_next()
-    with tf.Session() as sess:
-        sess.run(tf.tables_initializer())
-        sess.run(tf.global_variables_initializer())
-        sess.run(it.initializer)
-        actual = []
-        for i in range(3000):
-            actual.append(sess.run(next_example))
-    with open("retest_expected.pickle", "rb") as rtest_expected:
-        import pickle
-        expected = pickle.load(rtest_expected)
-
-    for (actual_features, actual_labels), (expected_features, expected_labels) in zip(actual, expected):
-        assert actual_features["inputs"] == approx(expected_features["inputs"])
-        assert (actual_features["length"] == expected_features["length"]).all()
-        assert (actual_labels["targets"] == expected_labels["targets"]).all()
